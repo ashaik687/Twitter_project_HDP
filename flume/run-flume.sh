21427d605aa353b9e17a6ffd2d3ac41ccf162a9f
ashaik687 <ashaik@engin.umass.edu>
2015-01-12 13:38:01 -0800

    configuration files

---
+++ flume/run-flume.sh
@@ -0,0 +1,2 @@
+echo "flume-ng agent -n TwitterAgent -c ~/sentiment/flume/conf -f ~/sentiment/flume/twitter.conf &"
+flume-ng agent -n TwitterAgent -c ~/sentiment/flume/conf -f ~/sentiment/flume/twitter.conf & 
